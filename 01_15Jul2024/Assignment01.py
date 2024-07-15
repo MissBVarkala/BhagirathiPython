512237d3f0578a99082315eeaef5be8780043145
MissBVarkala <bhagirathi.s.g05@gmail.com>
2024-07-15 20:20:40 +0530

    My first git hub push

---
+++ 01_15Jul2024/Assignment01.py
@@ -0,0 +1,12 @@
+name = input("User Name")
+Age = input("Enter your age")
+City = input ("Enter your city")
+PhoneNo = input ("Enter your PhoneNumber")
+#name,Age,City,PhoneNo = input ("Name","Age","City","PN") - This doesnot work
+
+
+# print(name,Age,City,PhoneNo) - This too works
+print(name)
+print(Age)
+print(City)
+print(PhoneNo)
